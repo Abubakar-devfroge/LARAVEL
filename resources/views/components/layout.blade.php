f09e876ef3b19b13bfa328689dd92a918bf7b86a
Abubakar devfroge <info.devforge@gmail.com>
2025-11-29 16:08:56 +0300

    second

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+    <meta name="csrf-token"
+          content="OK0RrpIuoI85wvLybeKjhVxlBszom5ZcEwr5XMBX">
+
+    <title>Laravel Partners</title>
+
+            
+        <meta property="og:title" content="Laravel Partners">
+    
+    <meta property="og:description" content="Find a Laravel-endorsed development partner to help with your next project. Find an expert based on your needs and reach out to start a conversation.">
+    <meta name="description" content="Find a Laravel-endorsed development partner to help with your next project. Find an expert based on your needs and reach out to start a conversation.">
+
+
+    <meta property="og:type" content="website">
+
+
+ <meta property="og:locale" content="en_US"> 
+ <meta property="og:image" content="https://partners.laravel.com/assets/images/laravel-partners-og.png"> 
+    <meta property="og:url" content="https://partners.laravel.com">
+    <link rel="canonical" href="https://partners.laravel.com">
+
+
+    <meta name="twitter:card" content="summary_large_image">
+ <meta name="twitter:creator" content="@laravelphp">  <meta name="twitter:site" content="@laravelphp">  <meta name="twitter:title" content="Laravel Partners">  <meta name="twitter:description" content="Find a Laravel-endorsed development partner to help with your next project. Find an expert based on your needs and reach out to start a conversation.">  <meta name="twitter:image" content="https://partners.laravel.com/assets/images/laravel-partners-og.png"> 
+    <!-- Fonts -->
+    <link rel="preconnect"
+          href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500"
+          rel="stylesheet" />
+
+    
+    <link rel="preconnect"
+          href="https://fonts.googleapis.com">
+    <link rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Geist+Mono&display=swap"
+          rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="preload" as="style" href="https://partners.laravel.com/build/assets/app-fe1268ae.css" /><link rel="stylesheet" href="https://partners.laravel.com/build/assets/app-fe1268ae.css" data-navigate-track="reload" />
+    <!-- Scripts -->
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer ></script>
+
+            <!-- Fathom - beautiful, simple website analytics -->
+        <script src="https://cdn.usefathom.com/script.js"
+                data-site="JCRQMBSH"
+                defer></script>
+        <!-- / Fathom -->
+
+    <!-- RudderStack SDK -->
+    <script src="https://partners.laravel.com/assets/js/rudderstack.js"></script>
+    <!-- Rudderstack SDK -->
+    
+    </head>
+
+<body class="bg-white font-sans antialiased">
+    {{ $slot}}
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,290 @@
-<h1>Test budaaa</h1>
+<x-layout>
+
+    <div>
+    <div
+         class="relative z-10 mx-auto flex max-w-screen-xl flex-row items-center justify-between space-x-2 p-4 sm:space-x-3 sm:p-7 xl:px-0">
+        <a href="/"
+           class="block">
+            <img class="h-5 md:h-6"
+                 src="/assets/images/laravel-partners.svg"
+                 alt="GOVDOC" />
+        </a>
+
+        <div class="flex w-auto items-center space-x-2">
+                            <a href="/register"
+                   id="browse-partners-btn"
+                   class="hidden rounded-sm border border-gray-300 px-4 py-1 font-medium hover:border-gray-400 md:inline-block">Signup</a>
+            
+            <a href="/login"
+               id="join-btn"
+               class="inline-block rounded-sm bg-red-600 px-4 py-1 font-medium text-white hover:bg-red-600/90">
+               Login
+            </a>
+        </div>
+    </div>
+</div>
 
 
-<h1>mimi ni noma si noma</h1>
 
 
-<a href="/about">Go to About</a>
+   <header class="mx-auto max-w-screen-xl p-4 sm:p-7 xl:px-0">
+    <div class="relative z-10 pt-8">
+        <h1 class="mb-4 text-4xl tracking-tight md:text-6xl">
+            Accelerate Productivity
+        </h1>
+
+        <p class="max-w-xl text-gray-700">
+            Collaborate seamlessly and deliver work faster from anywhere.
+        </p>
+
+        <div class="flex gap-x-2 mt-7">
+                            <a href="/partners"
+                                            id="find-partner-btn"
+                                        class="inline-block rounded-sm border border-gray-300 px-4 py-2 hover:border-gray-400">
+                        View All Partners
+                </a>
+            
+            <a href="/login"
+               class="inline-flex items-center rounded-sm bg-red-600 px-4 py-1 font-medium text-white hover:bg-red-600/90">
+              Match with a Partner
+            </a>
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+    <div class="px-4 mx-auto mt-6 max-w-screen-xl sm:px-7 xl:px-0">
+        <div class="relative w-full overflow-hidden">
+    <div class="grid items-center gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                    <a href="/partners/byte5"
+               class="relative flex min-h-[500px] flex-col justify-end overflow-hidden rounded-sm border-none bg-[#161615] px-5 pb-10 pt-32">
+
+                
+                <div class="absolute inset-0 w-full h-full pointer-events-none">
+                    <img src="/assets/images/tall-cards/card-byte5.png"
+                         alt="byte5"
+                         class="object-cover object-left-top w-full h-full">
+
+                    <div
+                         class="absolute inset-0 w-full bg-gradient-to-b from-transparent via-transparent to-[black]">
+                    </div>
+                </div>
+
+                
+                <div class="relative flex flex-col justify-between">
+                    <div>
+                        
+                                                <div class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-md ">
+                            <img src="/assets/partners/logos/byte5.min.svg"
+                                 alt="byte5"
+                                 class="w-full rounded-md">
+                        </div>
+
+                        
+                        <div class="mt-4 text-lg font-medium text-white">
+                            byte5
+                        </div>
+
+                        
+                        <div class="mt-3 text-sm text-gray-300 line-clamp-3">
+                            We are THE Laravel Powerhouse in Germany. Our unmatched experience in Laravel and mission-critical web projects ensures the success in even your toughest endeavours. As the only Laravel Diamond Partner in Germany our clients trust us with their valuable projects – and we deliver!
+                        </div>
+
+                        <div class="mt-3 text-xs text-[#a1a09a]">
+                            
+                        </div>
+                    </div>
+
+                    <div class="flex items-start gap-3 mt-auto">
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#00404e] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#d3f7ff]">
+                                Diamond
+                            </span>
+                        </span>
+
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#4e0a00] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#ff6b49]">
+                                Featured
+                            </span>
+                        </span>
+                    </div>
+
+                    
+                </div>
+            </a>
+                    <a href="/partners/curotec"
+               class="relative flex min-h-[500px] flex-col justify-end overflow-hidden rounded-sm border-none bg-[#161615] px-5 pb-10 pt-32">
+
+                
+                <div class="absolute inset-0 w-full h-full pointer-events-none">
+                    <img src="/assets/images/tall-cards/card-curotec.png"
+                         alt="Curotec"
+                         class="object-cover object-left-top w-full h-full">
+
+                    <div
+                         class="absolute inset-0 w-full bg-gradient-to-b from-transparent via-transparent to-[black]">
+                    </div>
+                </div>
+
+                
+                <div class="relative flex flex-col justify-between">
+                    <div>
+                        
+                                                <div class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-md ">
+                            <img src="/assets/partners/featured/curotec.jpg"
+                                 alt="Curotec"
+                                 class="w-full rounded-md">
+                        </div>
+
+                        
+                        <div class="mt-4 text-lg font-medium text-white">
+                            Curotec
+                        </div>
+
+                        
+                        <div class="mt-3 text-sm text-gray-300 line-clamp-3">
+                            Curotec is a premier software engineering firm known globally for our expertise in Laravel, and trusted by hundreds of clients from startup to Fortune 500s. With a deep focus on Laravel and the broader ecosystem, we’ve built a reputation for delivering high-performance, scalable applications that power mission-critical business functions.
+                        </div>
+
+                        <div class="mt-3 text-xs text-[#a1a09a]">
+                            
+                        </div>
+                    </div>
+
+                    <div class="flex items-start gap-3 mt-auto">
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#00404e] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#d3f7ff]">
+                                Diamond
+                            </span>
+                        </span>
+
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#4e0a00] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#ff6b49]">
+                                Featured
+                            </span>
+                        </span>
+                    </div>
+
+                    
+                </div>
+            </a>
+                    <a href="/partners/kirschbaum"
+               class="relative flex min-h-[500px] flex-col justify-end overflow-hidden rounded-sm border-none bg-[#161615] px-5 pb-10 pt-32">
+
+                
+                <div class="absolute inset-0 w-full h-full pointer-events-none">
+                    <img src="/assets/images/tall-cards/card-kir.png"
+                         alt="Kirschbaum"
+                         class="object-cover object-left-top w-full h-full">
+
+                    <div
+                         class="absolute inset-0 w-full bg-gradient-to-b from-transparent via-transparent to-[black]">
+                    </div>
+                </div>
+
+                
+                <div class="relative flex flex-col justify-between">
+                    <div>
+                        
+                                                <div class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-md ">
+                            <img src="/assets/partners/featured/kirschbaum.png"
+                                 alt="Kirschbaum"
+                                 class="w-full rounded-md">
+                        </div>
+
+                        
+                        <div class="mt-4 text-lg font-medium text-white">
+                            Kirschbaum
+                        </div>
+
+                        
+                        <div class="mt-3 text-sm text-gray-300 line-clamp-3">
+                            We are a team of carefully curated Laravel experts with a history of delivering practical and efficient solutions to complex problems. We bring products and services to market quickly by leveraging iterative processes and lean development techniques.
+                        </div>
+
+                        <div class="mt-3 text-xs text-[#a1a09a]">
+                            
+                        </div>
+                    </div>
+
+                    <div class="flex items-start gap-3 mt-auto">
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#00404e] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#d3f7ff]">
+                                Diamond
+                            </span>
+                        </span>
+
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#4e0a00] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#ff6b49]">
+                                Featured
+                            </span>
+                        </span>
+                    </div>
+
+                    
+                </div>
+            </a>
+                    <a href="/partners/tighten"
+               class="relative flex min-h-[500px] flex-col justify-end overflow-hidden rounded-sm border-none bg-[#161615] px-5 pb-10 pt-32">
+
+                
+                <div class="absolute inset-0 w-full h-full pointer-events-none">
+                    <img src="/assets/images/tall-cards/card-tighten.png"
+                         alt="Tighten"
+                         class="object-cover object-left-top w-full h-full">
+
+                    <div
+                         class="absolute inset-0 w-full bg-gradient-to-b from-transparent via-transparent to-[black]">
+                    </div>
+                </div>
+
+                
+                <div class="relative flex flex-col justify-between">
+                    <div>
+                        
+                                                <div class="flex h-10 w-10 items-center justify-center overflow-hidden rounded-md p-0">
+                            <img src="/assets/partners/featured/tighten.jpg"
+                                 alt="Tighten"
+                                 class="w-full rounded-md">
+                        </div>
+
+                        
+                        <div class="mt-4 text-lg font-medium text-white">
+                            Tighten
+                        </div>
+
+                        
+                        <div class="mt-3 text-sm text-gray-300 line-clamp-3">
+                            We&#039;re the experts in all things Laravel. If you need an app built or rescued, or you need your team built or leveled up, we&#039;ve seen more Laravel apps than anyone, and know what you need to do.
+                        </div>
+
+                        <div class="mt-3 text-xs text-[#a1a09a]">
+                            
+                        </div>
+                    </div>
+
+                    <div class="flex items-start gap-3 mt-auto">
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#00404e] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#d3f7ff]">
+                                Diamond
+                            </span>
+                        </span>
+
+                        <span class="inline-flex h-5 items-center rounded-sm bg-[#4e0a00] px-1.5 py-0">
+                            <span class="text-xs font-medium text-[#ff6b49]">
+                                Featured
+                            </span>
+                        </span>
+                    </div>
+
+                    
+                </div>
+            </a>
+            </div>
+</div>
+    </div>
+</x-layout>
